75c147875e085fd80d091bec88876898ebf15511
MehdiFarshbaf <mehdifarshbaf92@gmail.com>
2024-09-14 17:43:43 +0330

    about us

---
+++ src/components/home/AboutUs.jsx
@@ -0,0 +1,20 @@
+import AboutImage from './../../assets/images/about.png'
+import Logo from './../../assets/images/Logo.png'
+
+const AboutUs = () => {
+    return (
+        <section className="w-full flex flex-col justify-center items-center mb-36">
+            <p className="title-home sm:mb-4">درباره ما</p>
+            <div className="w-full flex justify-center items-center gap-[60px] sm:flex-col-reverse">
+                <div className='w-[50%] sm:w-full flex flex-col items-end gap-[26px] sm:items-center sm:justify-center'>
+                    <img src={Logo} alt="logo" className='w-[193px] h-[34px]' />
+                    <p className='font-noram text-sm text-black rtl text-justify'>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد</p>
+                </div>
+                <div className='w-[50%] sm:w-full sm:flex sm:justify-center sm:items-center'>
+                    <img src={AboutImage} alt="about" className='w-[417px] object-contain' />
+                </div>
+            </div>
+        </section>
+    )
+}
+export default AboutUs
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import AboutUs from "../components/home/AboutUs"
 import ContactUs from "../components/home/ContactUs"
 
 
 const Home = () => {
     return (
         <div>
+            <AboutUs/>
            <ContactUs/>
         </div>
     )
