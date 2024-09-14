412e43a9d72ab59ff3c6e68827cdee311ede3acc
MehdiFarshbaf <mehdifarshbaf92@gmail.com>
2024-09-14 18:04:00 +0330

    content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import AboutUs from "../components/home/AboutUs"
 import ContactUs from "../components/home/ContactUs"
+import Contentproduction from "../components/home/Contentproduction"
 
 
 const Home = () => {
     return (
         <div>
+            <Contentproduction/>
             <AboutUs/>
            <ContactUs/>
         </div>
